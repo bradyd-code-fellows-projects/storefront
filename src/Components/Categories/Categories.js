ea7921ba3382d3b53ddec8fba8856babab0c4242
Brady Davenport <brady.davenport@pm.me>
2022-08-02 12:16:20 -0600

    can now select category

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -1,16 +1,35 @@
+import { connect } from 'react-redux';
+import { selectCategory, reset } from '../../store/categories';
 import './Categories.scss';
 
-export default function Categories(props) {
-  const { categories } = props;
-  // const categories = ['electronics', 'tools'];
+export function Categories(props) {
+
+  const { activeCategory, categories } = props;
+
   return (
     <>
+    <h2>Active Category: {activeCategory}</h2>
       <section id='categories'>
         <p id='categoriesTitle'>Categories</p>
         {categories.map((category, idx) => (
-          <div key={`category-${idx}`}>{category}</div>
+          <button key={`category-${idx}`} onClick={() => props.selectCategory(category.name)}>{category.name}</button>
         ))}
       </section>
     </>
   )
 }
+
+const mapStateToProps = ({ categories }) => {
+  console.log(categories);
+  return {
+    categories: categories.categories,
+    activeCategory: categories.activeCategory
+  }
+}
+
+const mapDispatchToProps = {
+  selectCategory,
+  reset
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Categories);
